f84dbb9fbbf7a86b004a1116154fbad473c2f0f3
Ondřej Velíšek <ondrejvelisek@gmail.com>
2025-09-27 23:37:57 +0200

    fix netlify output dir 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
   plugins: [
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
     tanstackStart({
+      target: "netlify",
       customViteReactPlugin: true,
     }),
     netlify(),
     viteReact(),
   ],
-})
+});
 
-export default config
+export default config;
